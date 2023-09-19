30a303cb60a6d0edb4475a9dc861685452e9ceed
DonyStark <48925269+DonyStark@users.noreply.github.com>
2023-09-18 21:44:23 -0600

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import { Main } from 'next/document'
+import Image from 'next/image'
+
+export default function Home() {
+  return(
+    <main><h1>My name is Doniyor</h1></main>
+  )
+}
---
+++ app/week2.jsx
@@ -0,0 +1,13 @@
+import StudentInfo from '../StudentInfo';
+
+function Week2Page() {
+    return (
+        <div>
+            <h1>My Shopping List</h1>
+            <StudentInfo />
+            {/* You can add your shopping list items here. */}
+        </div>
+    );
+}
+
+export default Week2Page;
